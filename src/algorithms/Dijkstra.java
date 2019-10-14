7ea664a4753ed164714f5e995a5f08b5fb3cc7ef
oakenman <tom.suchel@gmail.com>
2019-10-14 19:47:29 +0200

    Interface shortest path

---
+++ src/algorithms/Dijkstra.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+import com.mxgraph.view.mxGraph;
+
+public class Dijkstra extends AbstractShortestPath implements ShortestPath {
+
+	public Dijkstra(mxGraph graph) {
+		super(graph);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void findShortestPath() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/controller/mainWindow/MainWindowController.java
+++ src/controller/mainWindow/MainWindowController.java
@@ -10,6 +10,7 @@ import com.mxgraph.model.mxCell;
 import com.mxgraph.model.mxIGraphModel;
 import com.mxgraph.view.mxGraph;
 
+import algorithms.AbstractShortestPath;
 import util.Serialize;
 import util.StyleSheet;
 import view.mainWindow.MainWindow;
@@ -20,54 +21,55 @@ public class MainWindowController {
 	@SuppressWarnings("unused")
 	private static MainWindow view;
 	private static mxGraph graph;
-	
+	private static AbstractShortestPath asp;
+
 	private static int start;
 	private static int end;
-	
+
 	private static String graphPath;
-	
+
 	/*===== BUILDER =====*/
 	public static void main(String[] args) {
 		start = -1;
 		end = -1;
 		graphPath = "";
-		
+
 		graph = new mxGraph();
 		graph.setStylesheet(new StyleSheet());
 		view = new MainWindow(graph);
 	}
-	
+
 	/*===== GETTERS AND SETTERS =====*/
 	public static mxGraph getGraph() {
 		return graph;
 	}
-	
+
 	public static int getStart() {
 		return start;
 	}
-	
+
 	public static int getEnd() {
 		return end;
 	}
-	
+
 	public static void setStart(int id) {
 		start = id;
 		redrawGraph();
 	}
-	
+
 	public static void setEnd(int id) {
 		end = id;
 		redrawGraph();
 	}
-	
+
 	public static String getGraphPath() {
 		return graphPath;
 	}
-	
+
 	public static void setGraphPath(String path) {
 		graphPath = path;
 	}
-	
+
 	/*===== METHODS =====*/
 	/**
 	 * Charge un graphe depuis un fichier *.grp
@@ -75,10 +77,10 @@ public class MainWindowController {
 	public static void loadGraph() {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileFilter(new FileNameExtensionFilter("Graph file", "grp"));
-		
+
 		// Ouverture d'une fenêtre de type "Ouvrir fichier"
 		int returnVal = fileChooser.showOpenDialog(new JFrame());
-		
+
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			// Si l'extension est correcte
@@ -92,40 +94,89 @@ public class MainWindowController {
 				System.out.println("Error : invalid extension");
 			}
 		} else {
-//			System.out.println("Graph wasn't loaded");
+			//			System.out.println("Graph wasn't loaded");
 		}	
 	}
-	
+
 	public static boolean containsVertex(int id) {
 		Object[] cells = graph.getChildVertices(graph.getDefaultParent());
 		for (Object c : cells)
 		{
-		    mxCell cell = (mxCell) c;
-		    if(id == (int)cell.getValue()) {
-		    	return true;
-		    }
+			mxCell cell = (mxCell) c;
+			if(id == (int)cell.getValue()) {
+				return true;
+			}
 		}
 		return false;
 	}
-	
+
 	public static void redrawGraph() {
 		Object[] cells = graph.getChildVertices(graph.getDefaultParent());
 		for (Object c : cells)
 		{
-		    mxCell cell = (mxCell) c;
-		    if((int)cell.getValue() == start) {
-		    	graph.getModel().setStyle(cell, "START");
-		    }
-		    else if((int)cell.getValue() == end) {
-		    	graph.getModel().setStyle(cell, "END");
-		    }
-		    else {
-		    	graph.getModel().setStyle(cell, "ROUNDED");
-		    }
+			mxCell cell = (mxCell) c;
+			if((int)cell.getValue() == start) {
+				graph.getModel().setStyle(cell, "START");
+			}
+			else if((int)cell.getValue() == end) {
+				graph.getModel().setStyle(cell, "END");
+			}
+			else {
+				graph.getModel().setStyle(cell, "ROUNDED");
+			}
 		}
 	}
+
+	/*===== PLAY BUTTONS =====*/
+	public void firstStep() {
+		graph = asp.getFirstStep();
+		view.setGraph(graph);
+	}
 	
+	public void lastStep() {
+		graph = asp.getLastStep();
+		view.setGraph(graph);
+	}
 	
+	public void previousStep() {
+		try {
+			graph = asp.getPreviousStep();
+			view.setGraph(graph);
+		}
+		catch(Exception e) {
+			// Cas où on est déjà au début
+		}
+	}
+
+	public void nextStep() {
+		try {
+			graph = asp.getNextStep();
+			view.setGraph(graph);
+		}
+		catch(Exception e) {
+			// Cas où on est déjà à la fin
+		}
+	}
+
+	public void playPause() {
+		try {
+			while(true) {
+				graph = asp.getNextStep();
+				view.setGraph(graph);
+				try {
+					Thread.sleep(1000);				// à changer par la valeur de vitesse
+				}  catch (InterruptedException e) {
+					// ...
+				}
+			}
+		}
+		catch(Exception e) {
+			// Cas où on est à la fin
+		}
+
+	}
+
+
 }
 
 
